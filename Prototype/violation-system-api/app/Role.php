6588651ac94b52db4efcd64a71c979b1f9308fb1
rdcgomez <zemogzner@gmail.com>
2019-08-22 14:08:24 +0800

    correction on fillables on Role model and added fillable attribute to ViolationType

--- Prototype/violation-system-api/app/Role.php
+++ Prototype/violation-system-api/app/Role.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
-      'type', 
+      'role_name', 
     ];
 }
