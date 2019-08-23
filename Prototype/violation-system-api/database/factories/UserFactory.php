a98119e097f5b9da03c75b5fa56937bf39451be1
rdcgomez <zemogzner@gmail.com>
2019-08-23 12:51:16 +0800

    created factory for User

--- Prototype/violation-system-api/database/factories/UserFactory.php
+++ Prototype/violation-system-api/database/factories/UserFactory.php
@@ -1,27 +1,18 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\User;
-use Illuminate\Support\Str;
-use Faker\Generator as Faker;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
+use App\User;
+use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'id_number' => $faker->idNumber,
+        'name' => $faker->name, 
+        'email' => $faker->email->unique(), 
+        'password' => $faker->password,
+        'phone_number' => $faker->e164PhoneNumber,
+        'role_id' => $faker->randomElement($array = array (1,2)),
+
     ];
 });
