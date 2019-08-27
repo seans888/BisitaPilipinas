240e49ea7ef746dc2ffd71b81a19412c4a0f5e8c
rdcgomez <zemogzner@gmail.com>
2019-08-27 22:16:37 +0800

    done setting the seed data

--- Prototype/violation-system-api/database/seeds/RolesTableSeeder.php
+++ Prototype/violation-system-api/database/seeds/RolesTableSeeder.php
@@ -11,6 +11,30 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+      DB::table('roles')->insert([
+        [
+          'role_name' => 'student',
+        ],
+        
+        [
+          'role_name' => 'professor',
+        ],
+        
+        [
+          'role_name' => 'security guard',
+        ],
+        
+        [
+          'role_name' => 'guidance counselor',
+        ],
+        
+        [
+          'role_name' => 'program director',
+        ],
+        
+        [
+          'role_name' => 'discipline officer',
+        ],
+      ]);
     }
 }
