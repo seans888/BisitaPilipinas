7b1dda1de724b170da0047d49fdde2233d9b0967
jlsolomon <jlsolomon@student.apc.edu.ph>
2019-09-17 09:36:39 +0800

    Removed to free space

--- Prototype/mypets-retrofit-master/AndroidStudioProjects/MyPets-App-master/README.md
+++
@@ -1,5 +0,0 @@
-# MyPets-App
-:cat: Android Pets App
-
-# Screenshot
-![Alt text](https://raw.githubusercontent.com/haerulmuttaqin/MyPets-App/master/screenshot-app.png "App Screenshot")
--- Prototype/mypets-retrofit-master/htdocs/demo_pets/connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-define('host', 'localhost');
-define('user', 'root');
-define('pass', '');
-define('db', 'pets_db');
-
-$conn = mysqli_connect(host, user, pass, db) or die('Unable to Connect');
-?>
